6db906c2b097d6d7f52473bf30ecc369a4ae661f
Andres Jerriel Sinabutar <andres.sinabutar@gmail.com>
2023-04-01 11:03:15 +0700

    create custom exception helper

---
+++ core/utils/custom_exception_helper.py
@@ -0,0 +1,18 @@
+# Import CustomException
+from core.exceptions import CustomException
+
+class CustomExceptionHelper:
+    @staticmethod
+    def get_exception_response(exception: CustomException, description: str) -> dict:
+        return {
+            "description": description, 
+            "content": {
+                "application/json": {
+                    "example": {
+                        "code": exception.code,
+                        "error_code": exception.error_code,
+                        "message": exception.message,
+                    }
+                }
+            }
+        }
