461024aeeea8c86dbfc375fa77572748f1768c11
salsabiilashifa11 <salsabiilashifa11@gmail.com>
2023-04-02 23:07:45 +0700

    added all required template schemas for search feature

--- app/search/schemas/advanced_search.py
+++ app/search/schemas/advanced_search.py
@@ -1,4 +1,12 @@
 from pydantic import BaseModel, Field
+from typing import List
+
+from app.search.enums import FilterOperator
+
+class FilterConditions(BaseModel):
+    key: str = Field(..., description="Entity name that will be evaluated")
+    operator: FilterOperator = Field(..., description="Operator that will evaluate key and value relation")
+    value: any = Field(..., description="Value that will be evaluated against key")
 
 class AdvancedSearchQuery(BaseModel):
-    pass
+    match: List[FilterConditions] = Field(..., description="List of fitler conditions that make up the advanced search query")
--- app/search/schemas/search_result.py
+++ app/search/schemas/search_result.py
@@ -1,4 +1,7 @@
 from pydantic import BaseModel, Field
 
 class MatchedDocument(BaseModel):
-    pass
+    id: str = Field(..., description="UUID of the matched document")
+    url: str = Field(..., description="Url leading to the path of the matched document in S3 object storage")
+    rank: int = Field(..., description="Final ranking of matched document based on search query for result ordering")
+    score: float = Field(..., description="Numeric score the matched document was given by the scoring algorithm based on the search query")
