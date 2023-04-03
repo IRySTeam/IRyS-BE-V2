d2e64796ebc6c378ef9c8ced6d873510904a187d
Andres Jerriel Sinabutar <andres.sinabutar@gmail.com>
2023-04-03 18:14:22 +0700

    complete refresh token logic

--- api/auth/auth.py
+++ api/auth/auth.py
@@ -2,8 +2,13 @@ from fastapi import APIRouter, Response
 
 from api.auth.request.auth import RefreshTokenRequest, VerifyTokenRequest
 from api.auth.response.auth import RefreshTokenResponse
-from app.auth.services.jwt import JwtService
-from app.user.schemas import ExceptionResponseSchema
+from app.auth.services.token import TokenService
+from core.utils import (
+    CustomExceptionHelper
+)
+from core.exceptions import (
+    InvalidTokenException,
+) 
 
 auth_router = APIRouter()
 
@@ -11,10 +16,10 @@ auth_router = APIRouter()
 @auth_router.post(
     "/refresh",
     response_model=RefreshTokenResponse,
-    responses={"400": {"model": ExceptionResponseSchema}},
+    responses={ "400": CustomExceptionHelper.get_exception_response(InvalidTokenException, "Invalid refresh token") },
 )
 async def refresh_token(request: RefreshTokenRequest):
-    token = await JwtService().create_refresh_token(
+    token = await TokenService().create_refresh_token(
         token=request.token, refresh_token=request.refresh_token
     )
     return {"token": token.token, "refresh_token": token.refresh_token}
@@ -22,5 +27,5 @@ async def refresh_token(request: RefreshTokenRequest):
 
 @auth_router.post("/verify")
 async def verify_token(request: VerifyTokenRequest):
-    await JwtService().verify_token(token=request.token)
+    await TokenService().verify_token(token=request.token)
     return Response(status_code=200)
