00c2cf4aca20845eeb4231e043bcc1bbb69cc8c7
Andres Jerriel Sinabutar <andres.sinabutar@gmail.com>
2023-04-08 17:09:19 +0700

    fix init files

--- core/fastapi/dependencies/__init__.py
+++ core/fastapi/dependencies/__init__.py
@@ -1,19 +1,2 @@
 from .logging import Logging
-from .permission import (
-    PermissionDependency,
-    IsAuthenticated,
-    IsAdmin,
-    AllowAll,
-    IsEmailNotVerified,
-    IsEmailVerified
-)
-
-__all__ = [
-    "Logging",
-    "PermissionDependency",
-    "IsAuthenticated",
-    "IsAdmin",
-    "AllowAll",
-    "IsEmailNotVerified",
-    "IsEmailVerified"
-]
+from .permission import *
