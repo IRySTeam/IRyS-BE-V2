4f4be6058dbbea96150f0a55686e7c78f86ec4b2
Andres Jerriel Sinabutar <andres.sinabutar@gmail.com>
2023-04-13 08:31:10 +0700

    add repository exceptions in app logic

--- api/repository/v1/repository.py
+++ api/repository/v1/repository.py
@@ -2,23 +2,41 @@ from fastapi import APIRouter, Depends, Query, Request
 
 from app.repository.schemas import (
     CreateRepositoryRequestSchema,
-    CreateRepositoryResponseSchema
+    CreateRepositoryResponseSchema,
 )
 from app.repository.services import RepositoryService
+from core.exceptions import (
+    UnauthorizedException,
+    EmailNotVerifiedException,
+    RepositoryDetailsEmptyException,
+)
 from core.fastapi.dependencies import (
     PermissionDependency,
     IsAuthenticated,
     IsEmailVerified,
 )
+from core.utils import CustomExceptionHelper
 
 repository_router = APIRouter()
+
 
 @repository_router.post(
     "/create",
     response_model=CreateRepositoryResponseSchema,
     responses={
+        "401": CustomExceptionHelper.get_exception_response(
+            UnauthorizedException, "Unauthorized"
+        ),
+        "403": CustomExceptionHelper.get_exception_response(
+            EmailNotVerifiedException, "Email not verified"
+        ),
+        "400": CustomExceptionHelper.get_exception_response(
+            RepositoryDetailsEmptyException, "Some repository details are empty"
+        ),
     },
     dependencies=[Depends(PermissionDependency([IsAuthenticated, IsEmailVerified]))],
 )
 async def create_repository(request: Request, body: CreateRepositoryRequestSchema):
-    return await RepositoryService().create_repository(user_id=request.user.id, **body.dict())
+    return await RepositoryService().create_repository(
+        user_id=request.user.id, **body.dict()
+    )
--- app/repository/services/repository.py
+++ app/repository/services/repository.py
@@ -1,15 +1,27 @@
 from app.repository.schemas import CreateRepositoryResponseSchema
 from core.db import Transactional
+from core.exceptions import RepositoryDetailsEmptyException
 from core.repository import RepositoryRepo
+
 
 class RepositoryService:
     repository_repo = RepositoryRepo()
 
     def __init__(self):
         ...
-    
-    @Transactional()
-    async def create_repository(self, user_id: int, name: str, description: str, is_public: bool) -> CreateRepositoryResponseSchema:
-        repo = await self.repository_repo.save(user_id=user_id, params={ "name": name, "description": description, "is_public": is_public }, role="Owner")
 
-        return CreateRepositoryResponseSchema(name=name, description=description, is_public=is_public)
+    @Transactional()
+    async def create_repository(
+        self, user_id: int, name: str, description: str, is_public: bool
+    ) -> CreateRepositoryResponseSchema:
+        if not name or not description or is_public is None:
+            raise RepositoryDetailsEmptyException
+        repo = await self.repository_repo.save(
+            user_id=user_id,
+            params={"name": name, "description": description, "is_public": is_public},
+            role="Owner",
+        )
+
+        return CreateRepositoryResponseSchema(
+            name=name, description=description, is_public=is_public
+        )
