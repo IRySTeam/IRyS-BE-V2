93e4439657295eb985dfe514b1cbfdc6a141001d
Andres Jerriel Sinabutar <andres.sinabutar@gmail.com>
2023-04-19 11:20:36 +0700

    add new exceptions

--- core/exceptions/repository.py
+++ core/exceptions/repository.py
@@ -11,3 +11,21 @@ class RepositoryNotFoundException(CustomException):
     code = 404
     error_code = "REPOSITORY__NOT_FOUND"
     message = "Repository not found"
+
+
+class InvalidRepositoryRoleException(CustomException):
+    code = 400
+    error_code = "REPOSITORY__INVALID_ROLE"
+    message = "Invalid repository role"
+
+
+class DuplicateCollaboratorException(CustomException):
+    code = 422
+    error_code = "REPOSITORY__DUPLICATE_COLLABORATOR"
+    message = "User is already a collaborator of this repository"
+
+
+class InvalidRepositoryCollaboratorException(CustomException):
+    code = 400
+    error_code = "REPOSITORY__INVALID_COLLABORATOR"
+    message = "Invalid collaborator"
