04d50cfbd85c8a52bd26ca4b2c138d5731c163c6
andhikarei <andhikareihan349@gmail.com>
2023-04-30 22:25:04 +0700

    clean file

--- api/search/v1/search.py
+++ api/search/v1/search.py
@@ -1,26 +1,20 @@
-import json
+from fastapi import APIRouter, File, UploadFile
 
-from typing import List
-
-from fastapi import APIRouter, HTTPException, UploadFile, File, Depends
-
-from app.search.services.search import SearchService
-from app.search.enums.search import DomainEnum
-from app.search.schemas.search import SemanticSearchRequest, SemanticSearchResponseSchema, DocumentDetails
 from app.document.services.document import DocumentService
-from app.document.schemas.document import DocumentResponseSchema
-from app.preprocess import PreprocessUtil
 from app.exception import BaseHttpErrorSchema
-
-from core.fastapi.dependencies import (
-    PermissionDependency,
-    IsAdmin,
-    IsAuthenticated,
+from app.preprocess import PreprocessUtil
+from app.search.enums.search import DomainEnum
+from app.search.schemas.search import (
+    DocumentDetails,
+    SemanticSearchRequest,
+    SemanticSearchResponseSchema,
 )
+from app.search.services.search import SearchService
 
 search_router = APIRouter()
 ss = SearchService(None, None, None)
 ds = DocumentService()
+
 
 @search_router.post(
     "/",
@@ -33,27 +27,32 @@ ds = DocumentService()
         }
     },
 )
-async def search(request: SemanticSearchRequest):  
-    doc_ids = [16, 17, 18] # TODO: Get from document repo
+async def search(request: SemanticSearchRequest):
+    doc_ids = [16, 17, 18]  # TODO: Get from document repo
 
-    result = ss.run_search(request.query, request.domain, request.advanced_filter, doc_ids)
-    retrieved_doc_ids = [doc.get('id') for doc in result]
-    retrieved_doc_details = await DocumentService().get_document_by_ids(retrieved_doc_ids)
-    
+    result = ss.run_search(
+        request.query, request.domain, request.advanced_filter, doc_ids
+    )
+    retrieved_doc_ids = [doc.get("id") for doc in result]
+    retrieved_doc_details = await DocumentService().get_document_by_ids(
+        retrieved_doc_ids
+    )
+
     result_list = []
     for i in range(len(result)):
         result_list.append(
             DocumentDetails(
                 details=retrieved_doc_details[i].__dict__,
                 preview=f"...{result[i].get('text')}...",
-                highlights=PreprocessUtil().preprocess(request.query)
+                highlights=PreprocessUtil().preprocess(request.query),
             )
         )
-    
+
     return SemanticSearchResponseSchema(
         message=f"Successfully retrieved {len(retrieved_doc_ids)} documents",
-        result=result_list
+        result=result_list,
     )
+
 
 @search_router.post(
     "/file",
@@ -67,11 +66,13 @@ async def search(request: SemanticSearchRequest):
     },
 )
 async def upload_document(domain: DomainEnum, file: UploadFile = File(...)):
-    doc_ids = [16, 17, 18] # TODO: Get from document repo
+    doc_ids = [16, 17, 18]  # TODO: Get from document repo
 
     result = ss.run_file_search(file, domain, doc_ids)
-    retrieved_doc_ids = [doc.get('id') for doc in result]
-    retrieved_doc_details = await DocumentService().get_document_by_ids(retrieved_doc_ids)
+    retrieved_doc_ids = [doc.get("id") for doc in result]
+    retrieved_doc_details = await DocumentService().get_document_by_ids(
+        retrieved_doc_ids
+    )
 
     result_list = []
     for i in range(len(result)):
@@ -79,11 +80,11 @@ async def upload_document(domain: DomainEnum, file: UploadFile = File(...)):
             DocumentDetails(
                 details=retrieved_doc_details[i].__dict__,
                 preview=f"...{result[i].get('text')}...",
-                highlights=[]
+                highlights=[],
             )
         )
-    
+
     return SemanticSearchResponseSchema(
         message=f"Successfully retrieved {len(retrieved_doc_ids)} documents",
-        result=result_list
+        result=result_list,
     )
--- app/search/schemas/elastic.py
+++ app/search/schemas/elastic.py
@@ -1,19 +1,41 @@
+from typing import Any, List
+
 from pydantic import BaseModel, Field
-from typing import List, TypedDict, Any
+
 
 class MatchedDocument(BaseModel):
     doc_id: int = Field(..., description="Internal ID of document in database")
     id: str = Field(..., description="UUID of the matched document")
-    score: float = Field(..., description="Numeric score the matched document was given by the scoring algorithm based on the search query")
+    score: float = Field(
+        ...,
+        description="Numeric score the matched document was given by the scoring algorithm based on the search query",
+    )
     title: str = Field(..., description="The title of the document")
-    preprocessed_text: str = Field(..., description="Preprocessed content of the document")
-    document_metadata: dict[Any, Any] = Field(..., description="Key value pair mappings of recognized extracted entities")
-    
+    preprocessed_text: str = Field(
+        ..., description="Preprocessed content of the document"
+    )
+    document_metadata: dict[Any, Any] = Field(
+        ..., description="Key value pair mappings of recognized extracted entities"
+    )
+
+
 class SearchResult(BaseModel):
-    result: List[MatchedDocument] = Field(..., description="Collection of matched documents from ElasticSearch")
+    result: List[MatchedDocument] = Field(
+        ..., description="Collection of matched documents from ElasticSearch"
+    )
+
 
 class MatchedDocumentDepr(BaseModel):
     id: str = Field(..., description="UUID of the matched document")
-    url: str = Field(..., description="Url leading to the path of the matched document in S3 object storage")
-    rank: int = Field(..., description="Final ranking of matched document based on search query for result ordering")
-    score: float = Field(..., description="Numeric score the matched document was given by the scoring algorithm based on the search query")
+    url: str = Field(
+        ...,
+        description="Url leading to the path of the matched document in S3 object storage",
+    )
+    rank: int = Field(
+        ...,
+        description="Final ranking of matched document based on search query for result ordering",
+    )
+    score: float = Field(
+        ...,
+        description="Numeric score the matched document was given by the scoring algorithm based on the search query",
+    )
