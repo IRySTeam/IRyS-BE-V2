bb09d6de58fed3ddd7179005f02008a6a435cfcf
Andres Jerriel Sinabutar <andres.sinabutar@gmail.com>
2023-05-17 16:01:16 +0700

    refactor endpoint in router files

--- api/document/v1/document.py
+++ api/document/v1/document.py
@@ -1,15 +1,24 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 
 from app.document.schemas import (
-    DocumentResponseSchema,
-    IncludeIndexQueryParams,
+    AddDocumentCollaboratorRequestSchema,
+    DocumentCollaboratorSchema,
+    DocumentDetailResponseSchema,
+    EditDocumentCollaboratorRequestSchema,
+    EditRepositoryDocumentRequestSchema,
+    RemoveDocumentCollaboratorRequestSchema,
 )
-from app.document.services import document_service
-from core.exceptions import BadRequestException, UnauthorizedException
+from app.document.services import DocumentService
+from app.repository.schemas import DocumentIdPathParams, MessageResponseSchema
+from core.exceptions import (
+    DocumentNotFoundException,
+    ForbiddenException,
+    UnauthorizedException,
+)
 from core.exceptions.user import EmailNotVerifiedException
-from core.fastapi.dependencies.permission import (
+from core.fastapi.dependencies import (
     IsAuthenticated,
     IsEmailVerified,
     PermissionDependency,
@@ -29,19 +38,143 @@ document_router = APIRouter(
 
 
 @document_router.get(
-    "",
-    description="Get all documents",
-    response_model=List[DocumentResponseSchema],
+    "/{document_id}",
+    description="Get a document by ID including metadata and content",
+    response_model=DocumentDetailResponseSchema,
     responses={
-        "400": CustomExceptionHelper.get_exception_response(
-            BadRequestException,
-            "Bad request, please check request body, params, headers, or query",
-        )
+        "403": CustomExceptionHelper.get_exception_response(
+            ForbiddenException, "Document indexing is not finished yet"
+        ),
+        "404": CustomExceptionHelper.get_exception_response(
+            DocumentNotFoundException, "Document not found"
+        ),
+    },
+)
+async def get_repo_document_by_id(
+    request: Request,
+    path: DocumentIdPathParams = Depends(),
+):
+    return await DocumentService().get_repository_document_by_id(
+        doc_id=path.document_id,
+        user_id=request.user.id,
+    )
+
+
+@document_router.post(
+    "/{document_id}/edit",
+    response_model=MessageResponseSchema,
+    responses={},
+    dependencies=[Depends(PermissionDependency([IsAuthenticated, IsEmailVerified]))],
+)
+async def edit_repository_document(
+    request: Request,
+    body: EditRepositoryDocumentRequestSchema,
+    path: DocumentIdPathParams = Depends(),
+):
+    await DocumentService().edit_repository_document(
+        user_id=request.user.id,
+        document_id=path.document_id,
+        name=body.name,
+        category=body.category,
+        is_public=body.is_public,
+    )
+
+    return MessageResponseSchema(message="Successful")
+
+
+@document_router.post(
+    "/{document_id}/delete",
+    description="Cascade delete a document",
+    response_model=MessageResponseSchema,
+    responses={
+        "404": CustomExceptionHelper.get_exception_response(
+            DocumentNotFoundException, "Document not found"
+        ),
     },
     dependencies=[Depends(PermissionDependency([IsAuthenticated, IsEmailVerified]))],
 )
-async def get_all_documents(query: IncludeIndexQueryParams = Depends()):
-    documents = await document_service.get_document_list(
-        include_index=query.include_index
+async def delete_repository_document_by_id(
+    request: Request,
+    path: DocumentIdPathParams = Depends(),
+):
+    await DocumentService().delete_repository_document(
+        user_id=request.user.id,
+        document_id=path.document_id,
     )
-    return [doc.__dict__ for doc in documents]
+    return MessageResponseSchema(message="Document deleted successfully")
+
+
+@document_router.get(
+    "/{document_id}/collaborators",
+    response_model=List[DocumentCollaboratorSchema],
+    responses={},
+    dependencies=[Depends(PermissionDependency([IsAuthenticated, IsEmailVerified]))],
+)
+async def get_document_collaborators(
+    request: Request,
+    document_id: int,
+):
+    return await DocumentService().get_document_collaborators(
+        user_id=request.user.id,
+        document_id=document_id,
+    )
+
+
+@document_router.post(
+    "/{document_id}/collaborators/add",
+    response_model=MessageResponseSchema,
+    responses={},
+    dependencies=[Depends(PermissionDependency([IsAuthenticated, IsEmailVerified]))],
+)
+async def add_document_collaborator(
+    request: Request,
+    document_id: int,
+    body: AddDocumentCollaboratorRequestSchema,
+):
+    await DocumentService().add_document_collaborator(
+        user_id=request.user.id,
+        document_id=document_id,
+        **body.dict(),
+    )
+
+    return MessageResponseSchema(message="Successful")
+
+
+@document_router.post(
+    "/{document_id}/collaborators/edit",
+    response_model=MessageResponseSchema,
+    responses={},
+    dependencies=[Depends(PermissionDependency([IsAuthenticated, IsEmailVerified]))],
+)
+async def edit_document_collaborator(
+    request: Request,
+    document_id: int,
+    body: EditDocumentCollaboratorRequestSchema,
+):
+    await DocumentService().edit_document_collaborator(
+        user_id=request.user.id,
+        document_id=document_id,
+        **body.dict(),
+    )
+
+    return MessageResponseSchema(message="Successful")
+
+
+@document_router.post(
+    "/{document_id}/collaborators/remove",
+    response_model=MessageResponseSchema,
+    responses={},
+    dependencies=[Depends(PermissionDependency([IsAuthenticated, IsEmailVerified]))],
+)
+async def remove_document_collaborator(
+    request: Request,
+    document_id: int,
+    body: RemoveDocumentCollaboratorRequestSchema,
+):
+    await DocumentService().delete_document_collaborator(
+        user_id=request.user.id,
+        document_id=document_id,
+        **body.dict(),
+    )
+
+    return MessageResponseSchema(message="Successful")
--- api/repository/v1/document.py
+++ api/repository/v1/document.py
@@ -2,38 +2,22 @@ from typing import List
 
 from fastapi import APIRouter, Depends, File, Request, UploadFile
 
-from app.document.schemas import (
-    AddDocumentCollaboratorRequestSchema,
-    DocumentCollaboratorSchema,
-    DocumentDetailResponseSchema,
-    DocumentResponseSchema,
-    EditDocumentCollaboratorRequestSchema,
-    RemoveDocumentCollaboratorRequestSchema,
-)
-from app.document.schemas.request import EditRepositoryDocumentRequestSchema
+from app.document.schemas import DocumentResponseSchema
 from app.document.services import DocumentService
 from app.repository.schemas import (
     AllDocumentDatabaseResponseSchema,
-    DocumentIdPathParams,
+    CountResponseSchema,
+    DocumentDatabaseQueryParams,
     RepositoryIdPathParams,
 )
-from app.repository.schemas.request import DocumentDatabaseQueryParams
-from app.repository.schemas.response import (
-    CountResponseSchema,
-    MessageResponseSchema,
-)
 from core.exceptions import (
+    BadRequestException,
     EmailNotVerifiedException,
+    NotFoundException,
     RepositoryNotFoundException,
     UnauthorizedException,
     UserNotAllowedException,
 )
-from core.exceptions.base import (
-    BadRequestException,
-    ForbiddenException,
-    NotFoundException,
-)
-from core.exceptions.document import DocumentNotFoundException
 from core.fastapi.dependencies import (
     IsAuthenticated,
     IsEmailVerified,
@@ -107,29 +91,6 @@ async def upload_multiple_document(
 
 
 @repository_document_router.get(
-    "/documents/{doc_id}",
-    description="Get a document by ID including metadata and content",
-    response_model=DocumentDetailResponseSchema,
-    responses={
-        "403": CustomExceptionHelper.get_exception_response(
-            ForbiddenException, "Document indexing is not finished yet"
-        ),
-        "404": CustomExceptionHelper.get_exception_response(
-            DocumentNotFoundException, "Document not found"
-        ),
-    },
-)
-async def get_repo_document_by_id(
-    request: Request,
-    path: DocumentIdPathParams = Depends(),
-):
-    return await DocumentService().get_repository_document_by_id(
-        doc_id=path.doc_id,
-        user_id=request.user.id,
-    )
-
-
-@repository_document_router.get(
     "/{repository_id}/documents/count",
     description="Get the number of documents in a repository",
     response_model=CountResponseSchema,
@@ -149,50 +110,6 @@ async def get_repository_documents_count(
             repository_id=path.repository_id,
         )
     )
-
-
-@repository_document_router.post(
-    "/documents/{doc_id}/edit",
-    response_model=MessageResponseSchema,
-    responses={},
-    dependencies=[Depends(PermissionDependency([IsAuthenticated, IsEmailVerified]))],
-)
-async def edit_repository_document(
-    request: Request,
-    body: EditRepositoryDocumentRequestSchema,
-    path: DocumentIdPathParams = Depends(),
-):
-    await DocumentService().edit_repository_document(
-        user_id=request.user.id,
-        document_id=path.doc_id,
-        name=body.name,
-        category=body.category,
-        is_public=body.is_public,
-    )
-
-    return MessageResponseSchema(message="Successful")
-
-
-@repository_document_router.post(
-    "/documents/{doc_id}/delete",
-    description="Cascade delete a document",
-    response_model=MessageResponseSchema,
-    responses={
-        "404": CustomExceptionHelper.get_exception_response(
-            DocumentNotFoundException, "Document not found"
-        ),
-    },
-    dependencies=[Depends(PermissionDependency([IsAuthenticated, IsEmailVerified]))],
-)
-async def delete_repository_document_by_id(
-    request: Request,
-    path: DocumentIdPathParams = Depends(),
-):
-    await DocumentService().delete_repository_document(
-        user_id=request.user.id,
-        document_id=path.doc_id,
-    )
-    return MessageResponseSchema(message="Document deleted successfully")
 
 
 @repository_document_router.get(
@@ -218,87 +135,3 @@ async def get_repository_documents_database(
         page_no=query.page_no,
         find_document=query.find_document,
     )
-
-
-@repository_document_router.get(
-    "/{repository_id}/documents/{document_id}/collaborators",
-    response_model=List[DocumentCollaboratorSchema],
-    responses={},
-    dependencies=[Depends(PermissionDependency([IsAuthenticated, IsEmailVerified]))],
-)
-async def get_document_collaborators(
-    request: Request,
-    repository_id: int,
-    document_id: int,
-):
-    return await DocumentService().get_document_collaborators(
-        user_id=request.user.id,
-        repository_id=repository_id,
-        document_id=document_id,
-    )
-
-
-@repository_document_router.post(
-    "/{repository_id}/documents/{document_id}/collaborators/add",
-    response_model=MessageResponseSchema,
-    responses={},
-    dependencies=[Depends(PermissionDependency([IsAuthenticated, IsEmailVerified]))],
-)
-async def add_document_collaborator(
-    request: Request,
-    repository_id: int,
-    document_id: int,
-    body: AddDocumentCollaboratorRequestSchema,
-):
-    await DocumentService().add_document_collaborator(
-        user_id=request.user.id,
-        repository_id=repository_id,
-        document_id=document_id,
-        **body.dict(),
-    )
-
-    return MessageResponseSchema(message="Successful")
-
-
-@repository_document_router.post(
-    "/{repository_id}/documents/{document_id}/collaborators/edit",
-    response_model=MessageResponseSchema,
-    responses={},
-    dependencies=[Depends(PermissionDependency([IsAuthenticated, IsEmailVerified]))],
-)
-async def edit_document_collaborator(
-    request: Request,
-    repository_id: int,
-    document_id: int,
-    body: EditDocumentCollaboratorRequestSchema,
-):
-    await DocumentService().edit_document_collaborator(
-        user_id=request.user.id,
-        repository_id=repository_id,
-        document_id=document_id,
-        **body.dict(),
-    )
-
-    return MessageResponseSchema(message="Successful")
-
-
-@repository_document_router.post(
-    "/{repository_id}/documents/{document_id}/collaborators/remove",
-    response_model=MessageResponseSchema,
-    responses={},
-    dependencies=[Depends(PermissionDependency([IsAuthenticated, IsEmailVerified]))],
-)
-async def remove_document_collaborator(
-    request: Request,
-    repository_id: int,
-    document_id: int,
-    body: RemoveDocumentCollaboratorRequestSchema,
-):
-    await DocumentService().delete_document_collaborator(
-        user_id=request.user.id,
-        repository_id=repository_id,
-        document_id=document_id,
-        **body.dict(),
-    )
-
-    return MessageResponseSchema(message="Successful")
